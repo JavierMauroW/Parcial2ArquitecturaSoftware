21a71d314cf33ce4a3b4c3b16dc08c55fe01d1ba
JavierMauroW <javiermauromontenegro@gmail.com>
2025-04-29 18:35:06 -0500

    Swagger

---
+++ src/main/java/Configuration/CorsConfigurer.java
@@ -0,0 +1,29 @@
+package Configuration;
+
+
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer {
+
+    @Bean(name = "customCorsConfigurer")
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/") // Permitir solicitudes desde el emulador de Android
+                        .allowedOrigins("http://10.0.2.2:8080") // emulador de Android
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+
+        };
+    }
+}
